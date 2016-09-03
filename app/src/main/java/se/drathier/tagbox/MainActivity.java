b265e13f40b92c6c042372429c6343b4dbc4699b
drathier <fille.haglund@hotmail.com>
2016-09-03 11:59:26 +0200

    WIP.

--- app/src/main/java/se/drathier/tagbox/MainActivity.java
+++ app/src/main/java/se/drathier/tagbox/MainActivity.java
@@ -1,10 +1,24 @@
 package se.drathier.tagbox;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +29,67 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        NdefMessage[] msgs;
+
+        Intent intent = getIntent();
+        Log.d("MainAct", intent.toString());
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    Log.d("rawMsg", rawMsgs[i].toString());
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+        }
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(getIntent().getAction())) {
+            Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+            MifareUltralight a = MifareUltralight.get(tagFromIntent);
+            Log.d("tag_tech", tagFromIntent.toString());
+
+            try {
+                a.connect();
+                Log.d("raw_json", gson.toJson(a.readPages(0)));
+                Log.d("raw_json", gson.toJson(a.readPages(1)));
+                Log.d("raw_json", gson.toJson(a.readPages(2)));
+                Log.d("raw_json", gson.toJson(a.readPages(4)));
+                Log.d("raw_json", gson.toJson(a.readPages(64)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    a.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+        //process the msgs array
+
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+    }
+
+    public byte[] read_all(MifareUltralight mul) throws IOException {
+        ArrayList<Byte> out = new ArrayList<Byte>();
+        int max = 0xDE;
+        for (int i = 0; i < max; i+=4) {
+            out.addAll(mul.readPages(i));
+        }
+        mul.readPages()
     }
 
     @Override
