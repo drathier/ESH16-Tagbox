927ae7b9aff1400e75ccd9299d4849d111e4273f
Anders Söderström <anders@quidbit.se>
2016-09-03 21:13:48 +0200

    save profile working

--- app/src/main/java/se/drathier/tagbox/EditTermsActivity.java
+++ app/src/main/java/se/drathier/tagbox/EditTermsActivity.java
@@ -1,7 +1,9 @@
 package se.drathier.tagbox;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class EditTermsActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class EditTermsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_terms);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == android.R.id.home) {
+            super.onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
